411aa1fc560a4ae85484932ac797f7dbe8e07688
Kevin Ahlstrom <ikeavin@gmail.com>
2021-01-20 16:26:47 -0700

    Update LabQuestion3-TruthTable.py

--- LabQuestion3-TruthTable.py
+++ LabQuestion3-TruthTable.py
@@ -3,48 +3,48 @@
 t = True;
 f = False;
 
-print(" P  |  Q  | P and Q"); 
-print(str(t)+ " | " + str(t) +" | " + str((t and t)));
-print(str(t)+ " | " + str(f) +" | " + str((t and f)));
-print(str(f)+ " | " + str(t) +" | " + str((f and t)));
-print(str(f)+ " | " + str(f) +" | " + str((f and f)));
+print(" P \t|  Q \t| P and Q"); 
+print(str(t)+ "\t| " + str(t) +"\t| " + str((t and t)));
+print(str(t)+ "\t| " + str(f) +"\t| " + str((t and f)));
+print(str(f)+ "\t| " + str(t) +"\t| " + str((f and t)));
+print(str(f)+ "\t| " + str(f) +"\t| " + str((f and f)));
 
 print(" ");
-print(" P  |  Q  | P or Q");
-print(str(t)+ " | " + str(t) +" | " + str(t or t));
-print(str(t)+ " | " + str(f) +" | " + str(t or f));
-print(str(f)+ " | " + str(t) +" | " + str(f or t));
-print(str(f)+ " | " + str(f) +" | " + str(f or f));
+print(" P \t|  Q \t| P or Q");
+print(str(t)+ "\t| " + str(t) +"\t| " + str(t or t));
+print(str(t)+ "\t| " + str(f) +"\t| " + str(t or f));
+print(str(f)+ "\t| " + str(t) +"\t| " + str(f or t));
+print(str(f)+ "\t| " + str(f) +"\t| " + str(f or f));
 
 print(" ");
-print(" P  |  Q  | If P, then Q");
+print(" P \t| Q \t| If P, then Q");
 if(t):
-    print(str(t)+ " | " + str(t) +" | " + str(t));
+    print(str(t)+ "\t| " + str(t) +"\t| " + str(t));
 else:
-    print(str(t)+ " | " + str(t) +" | " + str(f));
+    print(str(t)+ "\t| " + str(t) +"\t| " + str(f));
 
 if(t):
-    print(str(t)+ " | " + str(f) +" | " + str(t));
+    print(str(t)+ "\t| " + str(f) +"\t| " + str(f));
 else:
-    print(str(t)+ " | " + str(f) +" | " + str(f));
+    print(str(t)+ "\t| " + str(f) +"\t| " + str(f));
 
 if(f):
-    print(str(f)+ " | " + str(t) +" | " + str(t));
+    print(str(f)+ "\t| " + str(t) +"\t| " + str(f));
 else:
-    print(str(f)+ " | " + str(t) +" | " + str(f));
+    print(str(f)+ "\t| " + str(t) +"\t| " + str(t));
 
 if(f):
-    print(str(f)+ " | " + str(f) +" | " + str(t));
+    print(str(f)+ "\t| " + str(f) +"\t| " + str(f));
 else:
-    print(str(f)+ " | " + str(f) +" | " + str(f));
+    print(str(f)+ "\t| " + str(f) +"\t| " + str(t));
 
 print(" ");
-print(" P  |  Q  | P if and only if Q");
+print(" P \t| Q\t| P if and only if Q");
 
-print(str(t)+ " | " + str(t) +" | " + str(t == t));
-print(str(t)+ " | " + str(f) +" | " + str(t == f));
-print(str(f)+ " | " + str(t) +" | " + str(f == t));
-print(str(f)+ " | " + str(f) +" | " + str(f == f));
+print(str(t)+ "\t| " + str(t) +"\t| " + str(t == t));
+print(str(t)+ "\t| " + str(f) +"\t| " + str(t == f));
+print(str(f)+ "\t| " + str(t) +"\t| " + str(f == t));
+print(str(f)+ "\t| " + str(f) +"\t| " + str(f == f));
 
 
     
