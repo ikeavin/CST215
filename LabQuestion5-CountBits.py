4334a560d12025057aaec536cb1e0d6b7bf52f91
Kevin Ahlstrom <ikeavin@gmail.com>
2021-02-04 19:48:31 -0700

    Update LabQuestion5-CountBits.py

--- LabQuestion5-CountBits.py
+++ LabQuestion5-CountBits.py
@@ -5,18 +5,18 @@ def countBits(n):
     setBits = 0
     nAsBinary = ""
     while(n > 0):     
-        nAsBinary = nAsBinary + str(n%2)
+        nAsBinary = str(n%2) + nAsBinary
         if n % 2 == 1:
             setBits+=1
         n = n//2
-    print(f'{initialN} in binary: {nAsBinary}, Amount of set bits: {setBits}')
+    print(f'{initialN} in binary: {nAsBinary}, Amount of set bits: {setBits} ')
 
 
-print(countBits(1))
-print(countBits(2))
-print(countBits(3))
-print(countBits(4))
-print(countBits(7))
-print(countBits(8))
-print(countBits(63))
-print(countBits(64))
+countBits(1)
+countBits(2)
+countBits(3)
+countBits(4)
+countBits(7)
+countBits(8)
+countBits(63)
+countBits(64)
